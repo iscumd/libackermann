8bf3d7d81b1b89535a69775220b957b4c74666cf
Andrew Ealovega <Andrew@Ealovega.dev>
2023-03-09 18:32:31 -0500

    Add steering ratios

--- include/libackermann/steering_ratio.hpp
+++ include/libackermann/steering_ratio.hpp
@@ -1,3 +1,45 @@
 #pragma once
 
-//TODO steering -> virtual and inverse. Make a function lookup table indexed using the isc project you want the function for. These all have the signature (f32) -> f32
+namespace ack {
+    /// Steering ratios take steering wheel rad, and return ackermann wheel rad
+    typedef float (*Ratio)(float);
+
+    /// Enumeration of all ISC projects that have a steering ratio defined
+    enum class Project : uintmax_t {
+        Phoenix = 0
+    };
+
+    /// Array of steering ratios per project.
+    /// Index this with the projects enum.
+    constexpr Ratio steering_ratios[] = {
+            // Project Phoenix steering ratio as measured with the stock wheel and rack.
+            [](float steering) -> float { return 0.04572763f * steering + -0.014521139f; },
+    };
+
+    /// Array of inverse steering ratios per project.
+    /// Index this with the projects enum.
+    ///
+    /// This is needed since our ratios are actually a function.
+    constexpr Ratio inverse_steering_ratios[] = {
+            // Project Phoenix steering ratio as measured with the stock wheel and rack.
+            [](float ack) -> float { return (ack + 0.014521139f) / 0.04572763f; },
+    };
+
+    /// Gets the steering ratio function for a given project.
+    ///
+    /// \param project Project to get the steering ratio for.
+    /// \return A function pointer to the steering ratio. This is a function that takes the steering wheel input angle,
+    /// and returns the resulting virtual ackermann wheel angle.
+    Ratio get_steering_ratio(Project project) {
+        return steering_ratios[static_cast<uintmax_t>(project)];
+    }
+
+    /// Gets the inverse steering ratio function for a given project.
+    ///
+    /// \param project Project to get the inverse steering ratio for.
+    /// \return A function pointer to the inverse steering ratio. This is a function that takes the virtual ackermann wheel angle,
+    /// and returns the current steering wheel position.
+    Ratio get_inverse_steering_ratio(Project project) {
+        return inverse_steering_ratios[static_cast<uintmax_t>(project)];
+    }
+}
--- test/units.cpp
+++ test/units.cpp
@@ -10,6 +10,11 @@ TEST(ConversionTests, isomorphic) {
     TwistCommand t_comm{3.3233, 0.34};
     auto ack = ack::twist_to_ackermann(t_comm, 0.323);
     EXPECT_EQ(ack::ackermann_to_twist(ack, 0.323), t_comm);
-}
+} //TODO make test to see if the conversion is actually correct (we know tta is correct, and att is an exact inverse, so in theory this is correct, but it would be nice to verify one or the other)
 
-//TODO make test to see if the conversion is actually correct
+TEST(RatioTests, pheonix) {
+    // Just make sure we don't segfault for now
+    auto fn = ack::get_steering_ratio(ack::Project::Phoenix);
+    float ack = fn(10);
+    EXPECT_EQ(ack::get_inverse_steering_ratio(ack::Project::Phoenix)(ack), 10);
+}
